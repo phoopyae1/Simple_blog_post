72b1b18de9bbbbc86bfff2b826933a3eff8879c9
anbehindY <yanminthwin@gmail.com>
2025-02-20 23:36:51 +0700

    Change server port

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //routes
-app.use("/api/DetailPage", PostRoute);
+app.use("/api/posts", PostRoute);
 app.get('/', (req, res) => {
     res.send('Hello World ,this is nodemon kjkk');
 });
 mongoose.connect("mongodb+srv://admin:1nQEg5QATJVp6gFW@backenddb.jf9zj.mongodb.net/Users?retryWrites=true&w=majority&appName=BackendDB").then(db => {
     console.log('DB connected')
-    app.listen(3000, () => { console.log('Server is running on port 3000') });
+    app.listen(4999, () => { console.log('Server is running on port 4999') });
 }).catch(err => { console.log(err) });
